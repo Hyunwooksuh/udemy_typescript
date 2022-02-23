961c2b9d4f12ee0948a8122440df06b3bebfe70d
서현욱 <hyunwookkotra@gmail.com>
2022-02-23 15:57:45 +0900

    typescript section 1

---
+++ course-starting-project/app.ts
@@ -0,0 +1,14 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: "wook",
+  age: 30,
+  hobbies: ['sports', 'cooking'],
+}
+
+let favoriteActivities: string[];
+favoriteActivities = ['sports', 1];
+
+console.log(person.name);
